66f3f4301c8231a32a598c1c2a97beb353cc0998
Joan Ml. Gregorio P. <hometrix@MacBook-Pro.local>
2026-09-20 21:31:49 -0400

    update splash-monitor sha256 to 2a6ae85

--- Casks/splash-monitor.rb
+++ Casks/splash-monitor.rb
@@ -1,6 +1,6 @@
 cask "splash-monitor" do
   version "1.0.0-beta"
-  sha256 "61b47a0f5a9031ebb71949992eed6e49689665f27e412e4cc9ffb7fc5662f75a"
+  sha256 "2a6ae8512a0b1a31d0e7e2fc031f42082a9a86783ce6a6d0b171b0862880682f"
 
   url "https://github.com/hometrix/SplashMonitor/releases/download/v#{version}/SplashMonitor-#{version}.dmg"
   name "Splash Monitor"
